e08fd5406883da327f77241fad4a057eafca7895
Honwhy Wang <honwhy.wang@hotmail.com>
2025-05-13 23:10:22 +0800

    Update useBrowserExtensions.ts

--- composables/useBrowserExtensions.ts
+++ composables/useBrowserExtensions.ts
@@ -5,7 +5,7 @@ export const useBrowserExtensions = () => {
   const edgeUrl = 'https://microsoftedge.microsoft.com/addons/detail/lnfifinboijcjoalhfiaelocbipmfbje'
   const firefoxUrl = 'https://addons.mozilla.org/en-US/firefox/addon/%E5%85%AC%E4%BC%97%E5%8F%B7%E6%96%87%E7%AB%A0%E7%BC%96%E8%BE%91%E5%99%A8/'
 
-  const chromeInstalls = ref(840)
+  const chromeInstalls = ref(850)
   const edgeInstalls = ref(1000)
   const firefoxInstalls = ref(70)
 
